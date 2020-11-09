cdf1a350dac0216fe5feb07fafceb148b1a2ece4
Thor Anderson <ta.anderson@gmail.com>
2020-11-09 09:28:27 -0700

    Fixed numerous errors to get it to render

--- src/SortinghatList.js
+++ src/SortinghatList.js
@@ -1,16 +1,19 @@
-import React, { useState, useEffect } from 'react' 
+import React, { useState, useEffect } from 'react'
 //import supersData from '../src/data/batman.json'
 //import supersData2 from '../src/data/ironman.json'
 //import superheroData from '../src/data/superheroData.json'
 import SuperheroView from './SuperheroView'
 import axios from 'axios'
-import {List} from '@material-ui/core'
-import {LazyLoad} from 'react-lazyload'
-import {ListItem, ListItemAvatar, Avatar} from '@material-ui/core'
-import { Card, CardActionArea, CardContent, Typography} from '@material-ui/core'
-import {batman} from './assets/images/batman.jpg' 
-
-
+import { List } from '@material-ui/core'
+import LazyLoad from 'react-lazyload'
+import { ListItem, ListItemAvatar, Avatar } from '@material-ui/core'
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  Typography,
+} from '@material-ui/core'
+import { batman } from './assets/images/batman.jpg'
 
 const Loading = () => (
   <div>
@@ -18,94 +21,72 @@ const Loading = () => (
   </div>
 )
 
-
-
 const SortinghatList = () => {
-
-
-const [harrypotterData, setHarrypotterData] = useState({ loading: false, houses: [] })
-//const heroes = superheroData.results
-//commit this
-
-
-//const [superData, setSuperData] = useState({superHeroes: [] })
-//const [loading, setLoading] = useState({ loading: false})
-
-
-const fetchHouses = async () => {
-  setHarrypotterData({loading: true})
-  axios
-  .get(`https://www.potterapi.com/v1/houses`, {
-    headers: {'key': process.env.REACT_APP_HP_API_KEY }
+  const [harrypotterData, setHarrypotterData] = useState({
+    loading: false,
+    houses: [],
   })
-  .then(function (response) {
-  console.log(response.data) 
-  setHarrypotterData({
-      loading: false, 
-      houses: response.data[0].name
-    })
-  }).catch(function (error){ 
-      console.log(error)
-  })
-  }
-  
+  //const heroes = superheroData.results
+  //commit this
+
+  //const [superData, setSuperData] = useState({superHeroes: [] })
+  //const [loading, setLoading] = useState({ loading: false})
+
   useEffect(() => {
-  //fetchHouses()
+    const fetchHouses = async () => {
+      const result = await axios(
+        `https://www.potterapi.com/v1/houses?key=$2a$10$OYdflm8pD4OZkWnQdzIj6.TEJlf.coZ3eux5XCFVTfqNWIGsDNzoO`,
+      )
+
+      console.log(result.data)
+      setHarrypotterData({ loading: true, houses: result.data })
+    }
+    fetchHouses()
   }, [])
-  
 
-
-
-return (
-
-
-<div>
-
-<List dense className="hpList"> 
-    {harrypotterData.houses.map((house, index) => {
-    
-            return (
-              <LazyLoad 
+  return (
+    <div>
+      <List dense className='hpList'>
+        {harrypotterData.houses.map((house, index) => {
+          return (
+            <LazyLoad
               key={house.name + house.mascot}
               placeholder={<Loading />}
               height={200}
-              >
+            >
               <SuperheroView
-              houses={house}
-              key={house.headOfHouse + house.founder}
-              
-              >
-
-              </SuperheroView>
-              </LazyLoad>
-            )
-            })}
-            </List>
-
-<Card className="superheroCard">
-      <CardActionArea>
-      <ListItem>
-      <ListItemAvatar>
-            <Avatar className="superheroAvatar"
-            alt={'Batman'}
-            src={batman}
-            />
+                houses={house}
+                key={house.headOfHouse + house.founder}
+              ></SuperheroView>
+            </LazyLoad>
+          )
+        })}
+      </List>
+      <Card className='superheroCard'>
+        <CardActionArea>
+          <ListItem>
+            <ListItemAvatar>
+              <Avatar className='superheroAvatar' alt={'Batman'} src={batman} />
             </ListItemAvatar>
-            </ListItem>
-        <CardContent className="cardContentInfo">
-          <Typography variant="body2" color="textSecondary" component="p">
-          {harrypotterData.houses.name}
-          {harrypotterData.houses.mascot}
-          {harrypotterData.houses.headOfHouse}
-          {harrypotterData.houses.founder}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
-
-</div>
+          </ListItem>
+          <CardContent className='cardContentInfo'>
+            {harrypotterData.houses.map((house) => {
+              return (
+                <Typography
+                  variant='body2'
+                  color='textSecondary'
+                  component='p'
+                  key={house._id}
+                >
+                  {house.name}
+                </Typography>
+              )
+            })}
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </div>
   )
 }
 
-  export default SortinghatList
-
+export default SortinghatList
--- src/SuperheroView.js
+++ src/SuperheroView.js
@@ -1,15 +1,25 @@
-import React from 'react' 
+import React from 'react'
 //import supersData from '../src/data/batman.json'
 //import supersData2 from '../src/data/ironman.json'
 //import SortingHatList from '.SortinghatList'
 //import {List} from '@material-ui/core'
-import {LazyLoad} from 'react-lazyload'
-import {ListItem, ListItemAvatar, ListItemText, ListItemSecondaryAction, Checkbox, Avatar} from '@material-ui/core'
-import {Card, CardActionArea, CardContent, CardMedia, Typography} from '@material-ui/core'
-import batman from './assets/images/batman.jpg' 
-
-
-
+import LazyLoad from 'react-lazyload'
+import {
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  ListItemSecondaryAction,
+  Checkbox,
+  Avatar,
+} from '@material-ui/core'
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Typography,
+} from '@material-ui/core'
+import batman from './assets/images/batman.jpg'
 
 const Loading = () => (
   <div>
@@ -17,64 +27,49 @@ const Loading = () => (
   </div>
 )
 
-
-
 //const [superData, setSuperData] = useState({superHeroes: [] })
 //const [loading, setLoading] = useState({ loading: false})
 
-
-
-
 const SuperheroView = (props) => {
-const hero = props.hero
-//test commit
-//const superheroName = superhero.appearance === null ? '' : superhero.strength
+  //const hero = props.hero
+  console.log(props.houses)
+  const house = props.houses
+  //test commit
+  //const superheroName = superhero.appearance === null ? '' : superhero.strength
 
+  return (
+    <div>
+      <ListItem button>
+        <ListItemAvatar>
+          <LazyLoad once={true} alt='...'>
+            <Avatar alt={'Avatar'} placeholder={<Loading />} src={batman} />
+          </LazyLoad>
+        </ListItemAvatar>
+        <ListItemText primary={house.name} />
+        <ListItemSecondaryAction>
+          <Checkbox edge='end' />
+        </ListItemSecondaryAction>
+      </ListItem>
 
-
-return (
-<div>
-
-<ListItem button> 
-<ListItemAvatar>
-            <LazyLoad once={true} alt="...">
-            <Avatar 
-            alt={'Avatar'}
-            placeholder={<Loading />}
-            src={batman}
-            />
-            </LazyLoad>
-            </ListItemAvatar>
-           <ListItemText primary={`${hero.name}`} />
-            <ListItemSecondaryAction>
-            <Checkbox edge="end"/>
-
-            </ListItemSecondaryAction>
-            </ListItem>
-
-            <Card className="card">
-<CardActionArea>
-  <CardMedia className="CardImage"
-    image={batman}
-    title="Contemplative Reptile"
-  />
-  <CardContent>
-    <Typography gutterBottom variant="h5" component="h2">
-    <h1>Name:</h1>
-    </Typography>
-    <Typography variant="body2" color="textSecondary" component="p">
-      <p>ID:</p>
-    </Typography>
-  </CardContent>
-</CardActionArea>
-</Card>
-
-</div>
+      <Card className='card'>
+        <CardActionArea>
+          <CardMedia
+            className='CardImage'
+            image={batman}
+            title='Contemplative Reptile'
+          />
+          <CardContent>
+            <Typography gutterBottom variant='h5' component='h2'>
+              Name: {house.name}
+            </Typography>
+            <Typography variant='body2' color='textSecondary' component='p'>
+              ID: {house._id}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </div>
   )
-
 }
 
-
-
-  export default SuperheroView
-
+export default SuperheroView
